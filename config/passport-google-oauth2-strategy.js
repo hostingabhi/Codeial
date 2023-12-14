94c37178aa41860be13ce7b87fed8734ad90eacf
hostingabhi <abhishekprajapat423@gmail.com>
2023-12-14 22:51:27 +0530

    001

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -1,10 +1,9 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth').Strategy; // Change here
 const crypto = require('crypto');
 const User = require('../models/user');
-const env = require('./environment'); 
+const env = require('./environment');
 
-// Tell passport to use a new strategy for google login
 passport.use(new GoogleStrategy({
     clientID: env.google_client_id,
     clientSecret: env.google_client_secret,
@@ -12,16 +11,13 @@ passport.use(new GoogleStrategy({
 },
 async function(accessToken, refreshToken, profile, done) {
     try {
-        // Find a user
         const user = await User.findOne({ email: profile.emails[0].value });
 
         console.log(profile);
 
         if (user) {
-            // If found, set this user as req.user
             return done(null, user);
         } else {
-            // If not found, create the user and set it as req.user
             const newUser = await User.create({
                 name: profile.displayName,
                 email: profile.emails[0].value,
@@ -33,7 +29,6 @@ async function(accessToken, refreshToken, profile, done) {
         console.log('Error in google strategy-passport', err);
         return done(err);
     }
-}
-));
+}));
 
 module.exports = passport;
